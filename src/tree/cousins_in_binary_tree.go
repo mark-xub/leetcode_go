202dfeb7ec0a01df1c6d235f92c869b7981fae6e
mark.xub <mark.xub@alibaba-inc.com>
2019-02-22 17:56:32 +0800

    别人优雅的解法

--- src/tree/cousins_in_binary_tree.go
+++ src/tree/cousins_in_binary_tree.go
@@ -35,19 +35,19 @@ func isCousins(root *TreeNode, x int, y int) bool {
 	if root.Val == x || root.Val == y {
 		return false
 	}
-	nodeSlice := []*TreeNode{root}
+	leverNodes := []*TreeNode{root}
 	valueParentNodeMap := make(map[int]*TreeNode, 0)
 
 	for {
-		if len(nodeSlice) <= 0 {
+		if len(leverNodes) <= 0 {
 			return false
 		}
-		valueParentNodeMap, nodeSlice = getLevelNodeMap(nodeSlice)
-		valX, okx := valueParentNodeMap[x]
-		valY, oky := valueParentNodeMap[y]
-		if okx && oky && valX != valY {
+		valueParentNodeMap, leverNodes = getLevelNodeMap(leverNodes)
+		parentX, findX := valueParentNodeMap[x]
+		parentY, findY := valueParentNodeMap[y]
+		if findX && findY && parentX != parentY {
 			return true
-		} else if !okx && !oky {
+		} else if !findX && !findY {
 			continue
 		} else {
 			return false
@@ -56,18 +56,50 @@ func isCousins(root *TreeNode, x int, y int) bool {
 }
 
 func getLevelNodeMap(nodeSlice []*TreeNode) (map[int]*TreeNode, []*TreeNode) {
-	nodeLevel := make([]*TreeNode, 0)
+	levelNodes := make([]*TreeNode, 0)
 	valueParentNodeMap := make(map[int]*TreeNode, 0)
 	for _, v := range nodeSlice {
 		if v.Left != nil {
 			valueParentNodeMap[v.Left.Val] = v
-			nodeLevel = append(nodeLevel, v.Left)
+			levelNodes = append(levelNodes, v.Left)
 		}
 		if v.Right != nil {
 			valueParentNodeMap[v.Right.Val] = v
-			nodeLevel = append(nodeLevel, v.Right)
+			levelNodes = append(levelNodes, v.Right)
 
 		}
 	}
-	return valueParentNodeMap, nodeLevel
+	return valueParentNodeMap, levelNodes
 }
+
+//func isCousins(root *TreeNode, x int, y int) bool {
+//	depthX, parentX := lookup(root, x)
+//	depthY, parentY := lookup(root, y)
+//	return depthX == depthY && parentX != parentY
+//}
+//
+//func lookup(root *TreeNode, n int) (depth int, parent int) {
+//    if root.Val == n {
+//        return 0, 0
+//    }
+//    if root.Left != nil {
+//        if root.Left.Val == n {
+//            return 1, root.Val
+//        }
+//        dep, par := lookup(root.Left, n)
+//        if dep != 0 {
+//            return dep+1, par
+//        }
+//    }
+//    if root.Right != nil {
+//        if root.Right.Val == n {
+//            return 1, root.Val
+//        }
+//        dep, par := lookup(root.Right, n)
+//        if dep != 0 {
+//            return dep+1, par
+//        }
+//    }
+//    return 0, 0
+//
+//}
